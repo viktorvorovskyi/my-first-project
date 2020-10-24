54ac197d22c8dfa8808e2a010c4d1afbbf5d074b
viktorvorovskyi <viktor.vorovskyi@gmail.com>
2020-10-24 19:34:38 +0300

    added project files

---
+++ my-first-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header className='header'>
+    <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' />
+  </header>
+);
+}
+export default Header;
---
+++ my-first-project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <nav className='nav'>
+        <div><a href='http://localhost:3000/'>Profile</a></div>
+        <div><a href='http://localhost:3000/'>Messages</a></div>
+        <div><a href='http://localhost:3000/'>News</a></div>
+        <div><a href='http://localhost:3000/'>Music</a></div>
+        <div><a href='http://localhost:3000/'>Settings</a></div>
+      </nav>
+);
+}
+export default Navbar;
