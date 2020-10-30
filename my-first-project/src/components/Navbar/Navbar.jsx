615eb2f03b0da0c0e1b5396a3e41ad2c5727d757
viktorvorovskyi <viktor.vorovskyi@gmail.com>
2020-10-31 00:16:54 +0200

    added new files

--- my-first-project/src/components/Navbar/Navbar.jsx
+++ my-first-project/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import s from'./Navbar.module.css';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
-  return(
-    <nav className={s.nav}>
-        <div className={s.item}>Profile</div>
-        <div className={s.item}><a>Messages</a></div>
-        <div className={s.item}><a>News</a></div>
-        <div className={s.item}><a>Music</a></div>
-        <div className={s.item}><a>Settings</a></div>
-      </nav>
-);
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}><a href="/Profile">Profile</a></div>
+            <div className={s.item}><a href="/Dialogs">Messages</a></div>
+            <div className={s.item}><a href="/News">News</a></div>
+            <div className={s.item}><a href="/Music">Music</a></div>
+            <div className={s.item}><a href="/Settings">Settings</a></div>
+
+        </nav>
+    );
 }
 export default Navbar;
